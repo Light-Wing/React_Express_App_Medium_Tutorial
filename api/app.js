0dfa8b961acc49345e899a0cfb43f0f9052d67ab
Light-Wing <manknapp@gmail.com>
2021-05-25 16:43:52 -0400

    d

--- api/app.js
+++ api/app.js
@@ -30,7 +30,7 @@ app.use("/testAPI", testAPIRouter);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
   });
 
